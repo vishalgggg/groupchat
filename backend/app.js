5593f360423ba29454d4136f33744abcceb23c9e
arjunreddy20 <arjunkalvakadi>
2024-12-27 23:53:13 +0530

    cors used here

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,23 @@
 const express = require('express');
 const userRoutes = require('./routes/userRoutes');
 const path = require('path');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors({
+    origin:"*"
+}));
+
 app.use(express.json());
+
 
 app.use("/api",userRoutes);
 
-app.use(express.static(path.join(__dirname,"public")));
+app.use(express.static(path.join(__dirname,"../frontend/public")));
 
 app.get("/",(req,res) => {
-    res.sendFile(path.join(__dirname,"./public/index.html"));
+    res.sendFile(path.join(__dirname,"../frontend/public/index.html"));
 });
 
 app.get("/login",(req,res) => {
